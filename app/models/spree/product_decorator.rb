129a2175ea9b32d30c553d1c7f5bb52b9eb8a06a
fdelrio89 <fdelrio89@gmail.com>
2014-08-08 12:56:45 -0400

    Solucionado bug de categorias cuando header estaba capitalizado

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -11,7 +11,7 @@ module Spree
       
       (2..spreadsheet.last_row).each do |i|
         begin 
-          row = Hash[[header, spreadsheet.row(i)].transpose]
+          row = Hash[[header.map(&:downcase), spreadsheet.row(i)].transpose]
           product = find_or_create_by(name: row["name"].strip.split.map(&:capitalize)*' ')
           product.attributes = { description: row["description"] }
         
@@ -30,9 +30,8 @@ module Spree
           product.prototype_id = Spree::Prototype.find_or_create_by(name: row["prototype"].strip.mb_chars.capitalize.to_s).id if row["prototype"]
           
           taxonomies.each do |taxonomy_name|
-            taxonomy = Spree::Taxonomy.find_or_create_by(name: taxonomy_name.strip.mb_chars.capitalize.to_s)
-            
             unless row[taxonomy_name].blank?
+              taxonomy = Spree::Taxonomy.find_or_create_by(name: taxonomy_name.strip.mb_chars.capitalize.to_s)
               row[taxonomy_name].split('/').each do |taxon_name|
                 taxon = Spree::Taxon.find_or_create_by name: taxon_name.strip.mb_chars.capitalize.to_s  
                 taxon.parent = taxonomy.root unless taxon.parent
